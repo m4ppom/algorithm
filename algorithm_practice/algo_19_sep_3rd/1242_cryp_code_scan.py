982b9e908bf6c79b71afa894d0d2828cd0e8b536
marsa <m4ppom@gmail.com>
2019-09-16 23:18:01 +0900

    algorithm cryp|20190916

--- algorithm_practice/algo_19_sep_3rd/1242_cryp_code_scan.py
+++ algorithm_practice/algo_19_sep_3rd/1242_cryp_code_scan.py
@@ -49,7 +49,7 @@ def encryp2(i, j):
     return
 
 
-def encryp(i, j):
+def encryp3(i, j):
     global code_ratio, cnt
     if j > 0:
         if base[i][j] == 0:
@@ -94,6 +94,42 @@ def encryp(i, j):
 
     return
 
+def encryp(i, j):
+    global code_ratio
+    # list poping reverse
+    top = 3
+    while len(base[i]):
+        if len(base[i])==0:
+            break
+        if base[i][-1] == 0:
+            base[i].pop(-1)
+            continue
+        while num_top != -1:
+            if len(base[i])==1:
+                break
+            if code_ratio[0] != 0:
+                aa = sum(code_ratio) % 7
+                bb = sum(code_ratio) // 7
+                if aa == 0:
+                    for l in range(4):
+                        if code_ratio[l] % bb:
+                            break
+                    else:
+                        for nn in range(4):
+                            code_ratio[nn] //= bb
+                        calculate(code_ratio)
+                        code_ratio = [0] * 4
+            if top == -1:
+                top = 3
+            a = base[i].pop(-1)
+            code_ratio[top] += 1
+            if base[i][-1] != a:
+                top -= 1
+                continue
+
+
+    return
+
 
 def calculate(lst):  # 코드비율 리스트에 넣어서 8자리만드는거
     global number_list, num_top
@@ -118,7 +154,7 @@ def calculate(lst):  # 코드비율 리스트에 넣어서 8자리만드는거
     num_top -= 1
     if num_top == -1:
         # print(enc(number_list))
-        result.append([enc(number_list),i,number_list])
+        result.append([enc(number_list),i,number_list])  # if lst[3] == next then pass this idx
         # print('넘나 리스트한거', number_list)
         number_list = [0] * 8
         num_top = 7
@@ -151,10 +187,6 @@ for test_num in range(1, testcase+1):
     base = [0]*N
     cnt = 0  # 코드뽑아온 전체 길이
     idx = 0
-    number_list = [0]*8
-    num_top = 7
-    result = [0]
-    code_ratio = [0]*4  # 문제 제시에 비율 나와있으니 나중에 활용하려고
     for i in range(N):
         base[i] = list(map(str, input()))
     # 여기까지 받아오는 부분
@@ -190,19 +222,24 @@ for test_num in range(1, testcase+1):
                 for k in range(len(b)-1, -1, -1):
                     base[i].insert(j, b[k])
     temp = 10
+    result = [0]
     for i in range(N):
-        while temp > 6:
-            temp = len(base[i])-1
-            for j in range(temp, -1, -1):  # 뒤에서 부터 앞으로
-                if base[i][j] == 1:
-                    encryp(i, j)  # 1만나면 멈춰서 확인해줌
-                    temp = j
-                    print(temp)
-                    if sum(base[i][:temp]) == 0:
-                        temp = 0
-                    break
-                else:
-                    pass
+        number_list = [0] * 8
+        num_top = 7
+        code_ratio = [0] * 4  # 문제 제시에 비율 나와있으니 나중에 활용하려고
+        encryp(i,-1)
+        # while temp > 6:
+        #     temp = len(base[i])-1
+        #     for j in range(temp, -1, -1):  # 뒤에서 부터 앞으로
+        #         if base[i][j] == 1:
+        #             encryp(i, j)  # 1만나면 멈춰서 확인해줌
+        #             temp = j
+        #             print(temp)
+        #             if sum(base[i][:temp]) == 0:
+        #                 temp = 0
+        #             break
+        #         else:
+        #             pass
         # if cnt != 0:
         #     if idx - cnt > 7:
         #         for j in range(idx - cnt, -1, -1):
@@ -215,17 +252,37 @@ for test_num in range(1, testcase+1):
     # print('다다ㅏ다다답모으므므므으므으므', result)
     # print(result)
     summ = 0
-    if len(result) > 2:
-        for i in range(1, len(result)-1):
-            if base[result[i][1]] != base[result[i+1][1]] and base[result[i][2]] != base[result[i+1][2]]:
-                summ += result[i+1][0]
-        else:
-            summ += result[1][0]
-    elif len(result) ==2:
-        summ = result[1][0]
-    else:
-        summ = 0
-    print(result)
+    laalal = []
+    for i in range(1, len(result)):
+        if result[i][2] not in laalal:
+            summ += result[i][0]
+            laalal.append(result[i][2])
+    summ = 0
+    # while len(result) != 1:
+    for i in range(1,len(result)):
+        for j in range(i+1, len(result)):
+            if result[i][2] == result[j][2] and result[i][1]+1 == result[j][1]:
+                result[j][0] = 0
+    for i in range(1,len(result)):
+        summ += result[i][0]
+    # if len(result) > 2:
+    #     for i in range(1,len(result)):
+    #         if result[i][2] not in laalal:
+    #             summ += result[i][0]
+    #             laalal.append(result[i][2])
+    #     # while len(result) == 3:
+    #     #     if result[1][2] == result[2][2]:
+    #     #         result.pop(2)
+    #     #     else:
+    #     #         summ += result[1][0]
+    #             # result.pop(1)
+    #         # if result[i][0] != result[i+1][0] or result[i][2] != result[i+1][2]:
+    #     summ += result[1][0]
+    # elif len(result) ==2:
+    #     summ = result[1][0]
+    # else:
+    #     summ = 0
+    # print(result)
     print('#{} {}'.format(test_num, summ))
     # if not result[0]:
     #     print('0')
