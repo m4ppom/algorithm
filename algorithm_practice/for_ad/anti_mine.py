e30009453a4d0df3b909f8cd6ebe422010687c10
marsa <m4ppom@gmail.com>
2019-11-16 13:41:29 +0900

    a

--- algorithm_practice/for_ad/anti_mine.py
+++ algorithm_practice/for_ad/anti_mine.py
@@ -3,22 +3,23 @@ sys.stdin = open('mine.txt', 'r')
 import collections
 
 
-def bfs(i, j):
+def bfs(y, x):
     q = collections.deque()
-    q.append((i, j))
+    q.append((y, x))
     while q:
         i, j = q.popleft()
         for d in range(4):
             ii = i+dy[d]
             jj = j+dx[d]
-            if -1 < ii < N and -1 < jj < N and vst[ii][jj] == 0 and L[ii][jj] == '.':  #
-                q.append((jj, ii))
-                vst[ii][jj] = vst[i][j] + 1
+            if -1 < ii < N and -1 < jj < N:
+                if vst[ii][jj] == 0 and L[ii][jj] == '.':  #
+                    q.append((jj, ii))
+                    vst[ii][jj] = vst[i][j] + 1
             else:
                 continue
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0, 1, 1, -1, -1]
+dx = [0, 0, -1, 1, 1, -1, 1, -1]
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
@@ -29,6 +30,15 @@ for tc in range(1, T+1):
         for j in range(N):
             if L[i][j] == '.' and vst[i][j] == 0:
                 vst[i][j] = 1
-                bfs(i, j)
-                cnt += 1
+                for d in range(8):
+                    ii = i + dy[d]
+                    jj = j + dx[d]
+                    if -1 < ii < N and -1 < jj < N:
+                        if L[ii][jj] == '.':
+                            bfs(i, j)
+                            cnt += 1
+    # for i in range(N):
+    #     for j in range(N):
+    #         if L[i][j] == '.' and vst[i][j] == 0:
+    #             cnt += 1
     print('#%d %d' % (tc, cnt))
