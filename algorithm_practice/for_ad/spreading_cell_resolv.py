7594fdcad28446185527400a10c7cebc62e52485
marsa <m4ppom@gmail.com>
2019-12-29 18:14:29 +0900

    resolve cell prov

--- algorithm_practice/for_ad/spreading_cell_resolv.py
+++ algorithm_practice/for_ad/spreading_cell_resolv.py
@@ -5,16 +5,42 @@ sys.stdin = open('cell.txt', 'r')
 # 4 directions cell spreading
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
-def cell_spreading(i, j):
+
+
+def cell_spreading(i, j, life):
     for dir in range(4):
         search_dir_i = i + dy[dir]
         search_dir_j = j + dx[dir]
         if base[search_dir_i][search_dir_j] == 0:
+            cell_info.append((life, search_dir_i, search_dir_j, life))
+            base[search_dir_i][search_dir_j] = life
+    return
+
+
+def spreading():
+    for sp in range(len(cell_info)):
+        cell_info[sp][3] = cell_info[sp][3]-1
+        if cell_info[sp][3] == -1:
+            life = cell_info[0]
+            cell_spreading(cell_info[sp][1], cell_info[sp][2], life)
     return
 
 
 testcase = int(input())
 for testnum in range(testcase + 1 ):
-    N, M, K = int(input().split())
-    base =[0]*N
+    N, M, K = map(int, input().split())
+    cell_info = []
+    base = [[0]*(M+2*K+1) for _ in range(N+2*K+1)]
+    for _ in range(N):
+        temp = list(map(int, input().split()))
+        for num in range(len(temp)):
+            a = K+_
+            b = K+num
+            base[a][b] = temp[num]
+            cell_info.append([temp[num], a, b, temp[num]])
+    for time in range(K):
+        cell_info.sort(reverse=True)
+        spreading()
+
+
 
